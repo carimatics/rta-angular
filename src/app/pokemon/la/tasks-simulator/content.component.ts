86ec58fb84db5ee87ffa64b0e53370a7d6a4c1db
carimatics <github.carimatics@gmail.com>
2025-06-11 22:29:46 +0900

    Remove redundant height calculation in sidebar for cleaner layout

--- src/app/pokemon/la/tasks-simulator/content.component.ts
+++ src/app/pokemon/la/tasks-simulator/content.component.ts
@@ -29,7 +29,7 @@ import { TaskTableComponent } from './task-table.component';
     <div data-theme="pokemon-la" class="flex flex-col bg-surface-container">
       <app-header class="fixed top-0" />
       <div class="flex">
-        <aside class="flex h-[calc(100vh-56px)] w-80 flex-col">
+        <aside class="flex w-80 flex-col">
           <app-pokemon-list [pokedex]="service.pokedex()" (clickPokemon)="service.currentPokemonId.set($event.id)" />
         </aside>
         <main class="mx-2 flex flex-1 flex-col">
@@ -62,7 +62,7 @@ import { TaskTableComponent } from './task-table.component';
             </app-primary-container>
           </section>
         </main>
-        <aside class="flex h-[calc(100vh-56px)] w-60 flex-col">
+        <aside class="flex w-60 flex-col">
           <div class="mt-12 overflow-y-scroll">
             <ul class="w-60 p-1">
               @for (segment of timelineSegments(); track segment.id) {
